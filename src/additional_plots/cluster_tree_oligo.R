a808c9d16a488d0f13debbc9c17b9b1f9336d950
NadineBestard <nadine.bestard@ed.ac.uk>
2022-07-15 13:17:54 +0100

    change colours

--- src/additional_plots/cluster_tree_oligo.R
+++ src/additional_plots/cluster_tree_oligo.R
@@ -4,6 +4,7 @@ library(clustree)
 library(patchwork)
 
 project <- "fire-mice"
+source(here("src/colours.R"))
 
 sce <- readRDS(here("processed",project,"sce_oligo_clusters_01.RDS"))
 
@@ -24,14 +25,18 @@ sce$cluster_oligo_sorted_1.1201 <- sce$cluster_oligo_k120
 tree_reverse <- clustree(sce, prefix = "cluster_oligo_sorted_", edge_arrow = FALSE) +
   scale_color_discrete(name="clustering k", labels=c("150","120","100","10"))
 
-k100 <- plotTSNE(sce, colour_by = "cluster_oligo_k100", text_by="cluster_oligo_k100" ) +
-  theme( legend.position = "none") + ggtitle("Clustering k100")
+# resolutions 
+theme <-  theme( legend.position = "none") 
+colour <- scale_colour_manual(values = cols) 
+
+k100 <- plotTSNE(sce, colour_by = "cluster_oligo_k100", text_by="cluster_oligo_k100" ) + 
+  theme + colour + ggtitle("Clustering k100")
 k150 <- plotTSNE(sce, colour_by = "cluster_oligo_k150", text_by="cluster_oligo_k150" )+
-  theme( legend.position = "none") + ggtitle("Clustering k150")
+  theme + colour + ggtitle("Clustering k150")
 k120 <- plotTSNE(sce, colour_by = "cluster_oligo_k120", text_by="cluster_oligo_k120") +
-  theme( legend.position = "none") + ggtitle("Clustering k120")
+  theme + colour + ggtitle("Clustering k120")
 k10 <- plotTSNE(sce, colour_by = "cluster_oligo_k10", text_by="cluster_oligo_k10") +
-  theme( legend.position = "none") + ggtitle("Clustering k10")
+  theme + colour + ggtitle("Clustering k10")
 
 
 pdf(here("outs", project, "plots", "cluster_resolutions_and_tree.pdf"), height = 7, width = 15)
