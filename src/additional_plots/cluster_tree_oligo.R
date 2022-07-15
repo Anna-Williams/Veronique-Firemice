158f5937addf91bfaa888e3037ebb0313d1fb793
NadineBestard <nadine.bestard@ed.ac.uk>
2022-07-15 13:08:18 +0100

    add tiff and change force (different version scater)

--- src/additional_plots/cluster_tree_oligo.R
+++ src/additional_plots/cluster_tree_oligo.R
@@ -14,26 +14,30 @@ pdf(here("outs",project,"plots", "cluster_tree_oligo.pdf"), height = 10, width =
 print(tree)
 dev.off()
 
-# test custom reverse tree, by addind number for ordering before and a 1 to keep 0s
+# test custom reverse tree, by adding number for ordering before and a 1 to keep 0s
 sce$cluster_oligo_sorted_3.101 <- sce$cluster_oligo_k10
 sce$cluster_oligo_sorted_0.1501 <- sce$cluster_oligo_k150
 sce$cluster_oligo_sorted_2.1001 <- sce$cluster_oligo_k100
 sce$cluster_oligo_sorted_1.1201 <- sce$cluster_oligo_k120
 
-#this was not helpful here.
+
 tree_reverse <- clustree(sce, prefix = "cluster_oligo_sorted_", edge_arrow = FALSE) +
   scale_color_discrete(name="clustering k", labels=c("150","120","100","10"))
 
-k100 <- plotTSNE(sce, colour_by = "cluster_oligo_k100", text_by="cluster_oligo_k100", force=0 ) +
+k100 <- plotTSNE(sce, colour_by = "cluster_oligo_k100", text_by="cluster_oligo_k100" ) +
   theme( legend.position = "none") + ggtitle("Clustering k100")
-k150 <- plotTSNE(sce, colour_by = "cluster_oligo_k150", text_by="cluster_oligo_k150", force=0 )+
+k150 <- plotTSNE(sce, colour_by = "cluster_oligo_k150", text_by="cluster_oligo_k150" )+
   theme( legend.position = "none") + ggtitle("Clustering k150")
-k120 <- plotTSNE(sce, colour_by = "cluster_oligo_k120", text_by="cluster_oligo_k120", force=0 ) +
+k120 <- plotTSNE(sce, colour_by = "cluster_oligo_k120", text_by="cluster_oligo_k120") +
   theme( legend.position = "none") + ggtitle("Clustering k120")
-k10 <- plotTSNE(sce, colour_by = "cluster_oligo_k10", text_by="cluster_oligo_k10", force=0 ) +
+k10 <- plotTSNE(sce, colour_by = "cluster_oligo_k10", text_by="cluster_oligo_k10") +
   theme( legend.position = "none") + ggtitle("Clustering k10")
 
 
 pdf(here("outs", project, "plots", "cluster_resolutions_and_tree.pdf"), height = 7, width = 15)
+((k10+k100)/(k120+k150))  | tree_reverse
+dev.off()
+
+tiff(here("outs", project, "plots", "cluster_resolutions_and_tree.tiff"), height = 7, width = 15, res = 300, units = "in")
 ((k10+k100)/(k120+k150))  | tree_reverse
 dev.off()
